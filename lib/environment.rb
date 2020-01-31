eb18d8530195a926bab3aeef300397925970a53a
howardbdev <howarddevennish@gmail.com>
2020-01-31 12:15:38 -0500

    turn on ActiveRecord logger

--- lib/environment.rb
+++ lib/environment.rb
@@ -4,6 +4,9 @@ require 'active_record'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.sqlite3')
 
+# show each SQL query in the console
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
 require_relative "./pokemon_trainer/version"
 require_relative "./pokemon_trainer/cli"
 require_relative "./pokemon_trainer/pokemon"
